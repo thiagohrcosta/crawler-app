97c7216d511b586a9983f53c388493c2691d7412
ThiagoHRCosta <thiagohrcosta86@gmail.com>
2023-04-12 11:41:02 -0300

    Adding nokogiri to parse data

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -16,9 +16,49 @@ class LeadsController < ApplicationController
     html_doc = Nokogiri::HTML.parse(html_file)
 
     a = params[:lead][:file].read
+    
+    raw_data = []
+
     a.split("\r\n").each do |line|
-      puts line
+      raw_data << line.split("\t")
     end
+
+    raw_data = raw_data[36...44].flatten.join.split("<p>").split("</p>")
+    raw_data = raw_data.flatten
+    binding.pry 
+
+    formatted_data = {
+      name: nil,
+      phone: nil,
+      message: nil,
+      selected_vehicle: nil,
+      price: nil,
+      year: nil,
+      link: nil
+    }
+
+    name = raw_data.each_with_index do |x, i|
+      next if x == nil
+
+      if x.include? "phone"
+        formatted_data[:phone] =  x.split("phone:").last.split("</p>")[0].strip
+      elsif x.include?  "name"
+        formatted_data[:name] = x.split("name:").last.strip.split("</p>")[0].strip
+      elsif x.include? "vehicle"
+        formatted_data[:selected_vehicle] = x.split("vehicle:").last.split("</p>")[0].strip
+      elsif x.include? "price"
+        formatted_data[:price] = x.split("price:").last.split("</p>")[0].strip
+      elsif x.include? "year"
+        formatted_data[:year] = x.split("year:").last.split("</p>")[0].strip
+      elsif x.include? "link"
+        formatted_data[:link] = x.split("link:").last.split("</p>")[0].strip
+      else
+        formatted_data[:message] = raw_data[-2].split("</p>")[0].strip
+      end
+    end
+
+    puts formatted_data    
+    binding.pry
   end
 
   private
---
+++ app/jobs/lead_generator_job.rb
@@ -0,0 +1,8 @@
+class LeadGeneratorJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+    
+  end
+end
